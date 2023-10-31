bd51d0fb04f8210064ad67701a820f368f71e3fe
Petter <petter.gustafsson@hotmail.com>
2023-10-31 11:21:22 +0100

    Much changes

---
+++ Ölprovning/BeerView/EditBeerView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBeerView.swift
+//  Ölprovning
+//
+//  Created by Petter Gustafsson on 2023-10-26.
+//
+
+import SwiftUI
+
+struct EditBeerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBeerView()
+}

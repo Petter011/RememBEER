fda508158bbda1389eeeb74f8b8e92e040d89a74
Petter <petter.gustafsson@hotmail.com>
2023-11-02 18:33:49 +0100

    UI/UX

---
+++ Ölprovning/BeerView/TheBestView.swift
@@ -0,0 +1,18 @@
+//
+//  TheBestView.swift
+//  Ölprovning
+//
+//  Created by Petter Gustafsson on 2023-10-31.
+//
+
+import SwiftUI
+
+struct TheBestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TheBestView()
+}

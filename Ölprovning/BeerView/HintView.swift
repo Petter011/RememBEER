1ae509250d6497e761ec25112c23a98b6cb8ddf8
Petter <petter.gustafsson@hotmail.com>
2023-12-13 16:13:01 +0100

    New view 'All Beers', new navBar, search function

---
+++ Ölprovning/BeerView/HintView.swift
@@ -0,0 +1,18 @@
+//
+//  HintView.swift
+//  RememBEER
+//
+//  Created by Petter Gustafsson on 2023-12-12.
+//
+
+import SwiftUI
+
+struct HintView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HintView()
+}
